cb598dc4fa30ed8d7ce406230d583d85457cb44e
Lucca Raphael <luccaraphae@gmail.com>
2021-10-22 23:01:38 -0300

    c0 - First commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'luccaraphae',
+  discord: 'luccaraphae#3029',
+  instagram: 'miixangel',
+  linkedin: 'luccaraphae',
+  twitter: 'Miixangel'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialMedia.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userLink.href = data.html_url
+      bioProfile.textContent = data.bio
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getProfileInfos()
